56a9fcbc73bffa7175578522625e94c090fbfa73
onurcatik <catikonur@gmail.com>
2025-02-17 22:23:05 +0300

    repo fixed

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,8 +1,26 @@
+"use client"
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Logo = () => {
-  return <Image height={60} width={60} alt="Logo" src="/images/favicon.ico" />;
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/");
+  };
+
+  return (
+    <Image
+      height={60}
+      width={60}
+      alt="Logo"
+      src="/images/favicon.ico"
+      onClick={handleClick}
+      style={{ cursor: "pointer" }}
+    />
+  );
 };
 
 export default Logo;
